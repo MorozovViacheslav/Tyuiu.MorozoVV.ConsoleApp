20f607befaa70731bf62f31a58ef23ed2b39939b
MorozovViacheslav <morozov1990_72@mail.ru>
2023-09-14 00:59:02 +0500

    We have fixed some mistakes

---
+++ Tyuiu.MorozovVV.ConsoleApp.Lab1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MorozovVV.ConsoleApp.Lab1.V1.Lib;
+
+namespace Tyuiu.MorozovVV.ConsoleApp.Lab1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpressionV_1_1()
+        {
+            double x = 5;
+            double y = 6;
+            double a = 9;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_1_1(x, y, a);
+
+            result = Math.Round(result, 6);
+            double wait = 534.387395;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_1_2()
+        {
+            double x = 4;
+            double y = -3;
+            double a = 7;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_1_2(x, y, a);
+
+            result = Math.Round(result, 8);
+            double wait = 19.78571429;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_5_1()
+        {
+            double x = 4;
+            double y = 9;
+            double a = -9;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_3_1(x, y, a);
+
+            result = Math.Round(result, 13);
+            double wait = 31.7987654320988;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_5_2()
+        {
+            double x = 4;
+            double y = 9;
+            double a = -9;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_3_2(x, y, a);
+
+            result = Math.Round(result, 12);
+            double wait = 84.634479717813;
+
+            Assert.AreEqual(result, wait);
+
+        }
+    }
+}
---
+++ Tyuiu.MorozovVV.ConsoleApp.Lab1.V1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MorozovVV.ConsoleApp.Lab1.V1.Lib;
+
+namespace Tyuiu.MorozovVV.ConsoleApp.Lab1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Лабораторная работа #1 | Выполнил: Морозов В. В. | НТм-22-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Лабораторная работа #1                                                  *");
+            Console.WriteLine("* Расчёты арифметических и математических выражений в C#                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Морозов Вячеслав Валерьевич | НТм-22-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на С#, которая выполняет расчёты          *");
+            Console.WriteLine("* арифметических и математических выражений.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x = 4;
+            double y = 9;
+            double a = -9;
+
+            Console.WriteLine("x = "+x);
+            Console.WriteLine("y = "+y);
+            Console.WriteLine("a = "+a);
+
+            DataService ds = new DataService();
+
+            double result1 = ds.SolveExpressV_3_1(x, y, a);
+            double result2 = ds.SolveExpressV_3_2(x, y, a);
+
+            result1 = Math.Round(result1, 13);
+            result2 = Math.Round(result2, 12);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" 1-е выражение: "+ result1);
+            Console.WriteLine(" 2-е выражение: "+ result2);
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.ReadKey();
+        }
+    }
+}
