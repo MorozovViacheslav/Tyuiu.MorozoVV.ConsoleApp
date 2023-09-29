28272cdc28421320f09f3aa7ec0b2bc482f71753
MorozovViacheslav <morozov1990_72@mail.ru>
2023-09-29 20:33:22 +0500

    Lab2 is done

---
+++ Tyuiu.MorozovVV.ConsoleApp.Lab2.V1.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MorozovVV.ConsoleApp.Lab2.V3.Lib
+{
+    public class DataService
+    {
+        public double SolveExpressV_3_1(double x, double y) 
+        {
+            double result = (3 + Math.Exp(y - 1)) / (1 + Math.Pow(x, 2) * Math.Abs(y - Math.Tan(x)));
+            return result;
+        }
+
+        public double SolveExpressV_3_2(double x, double y)
+        {
+            double result = (Math.Pow(y, 2) - Math.Cos(Math.Pow(x, 2)) + 10) / (Math.Pow(x, 2) - Math.Sin(Math.Pow (y, 2)) + 12);
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.MorozovVV.ConsoleApp.Lab2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MorozovVV.ConsoleApp.Lab2.V3.Lib;
+
+namespace Tyuiu.MorozovVV.ConsoleApp.Lab2.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpressionV_3_1()
+        {
+            double x = 5;
+            double y = 6;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_3_1(x, y);
+
+            result = Math.Round(result, 15);
+            double wait = 0.642908201949371;
+
+            Assert.AreEqual(result, wait);
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_3_2()
+        {
+            double x = 5;
+            double y = 6; ;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_3_2(x, y);
+
+            result = Math.Round(result, 14);
+            double wait = 1.18469833596795;
+
+            Assert.AreEqual(result, wait);
+        }
+    }
+}
