b0f12fa245630ae4b1c933402a3bc0eb3d648ce4
MorozovViacheslav <morozov1990_72@mail.ru>
2023-10-18 20:26:58 +0500

    Lab4 is done

---
+++ Tyuiu.MorozoVV.ConsoleApp.Lab4.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MorozoVV.ConsoleApp.Lab4.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MorozoVV.ConsoleApp.Lab4.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("43ba9319-1a96-409f-bf67-46dbc79e87eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
