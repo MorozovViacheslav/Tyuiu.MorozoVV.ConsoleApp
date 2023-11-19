c805ce1eb11ba708f703d6993f5e68b2249268cb
MorozovViacheslav <morozov1990_72@mail.ru>
2023-11-19 21:21:25 +0500

    Lab7 is done

---
+++ Tyuiu.MorozovVV.ConsoleApp.Lab7.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MorozovVV.ConsoleApp.Lab7.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MorozovVV.ConsoleApp.Lab7.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c3fba3b-4135-447f-a176-3fa1c51be63a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
